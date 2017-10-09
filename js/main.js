213b0b9d9b328fe8acf34fe1b81981c1194bd1d8
Patrick Finazzo <pfinazzoproduction@gmail.com>
2017-10-09 15:21:41 -0700

    cleaned up code, took out unnecesary psuedocode etc

--- js/main.js
+++ js/main.js
@@ -11,30 +11,17 @@ $(function () {
     [0, 0, 0, 0, 0, 0, 0]
   ];
 
-  // var player1color = "yellow";
-  // var player2color = "blue";
   var player1color = "red";
   var player2color = "blue";
   var player1 = 1;
   var player2 = 2;
   var turn;
-  // var blankspace = null;
-  
 
-  
-  
   /*----- cached element references -----*/
-
-
-
-
 
 
   /*----- event listeners -----*/
   $('table').on('click', 'td', click);
-
-
-
 
 
   /*----- functions -----*/
@@ -52,18 +39,15 @@ $(function () {
   };
 
 
-
   function click(event) {
     var cellTarget = $(event.target); // event target's cell
     var column = cellTarget.attr("data-col"); // event target's column
     var row = cellTarget.parent().attr("data-row"); // event target's row
-    row = parseInt(row, 10);
-    column = parseInt(column, 10);
-  board[row][column] === (1 || undefined) || board[row][column] === (2 || undefined) ? board[row][column] = turn : board[row][column] = turn;
-  (board[row][column] === 1) ? $(this).css({ "background-color": `${player1color}`}) : $(this).css({ "background-color": `${player2color}`})
-    switchTurn();
-  };
-
+    board[row][column] === 1 || board[row][column] === 2 ? board[row][column] = turn : board[row][column] = turn;
+    (board[row][column] === 1) ? $(this).css({ "background-color": `${player1color}`}) : $(this).css({ "background-color": `${player2color}`});
+      switchTurn();
+    };
+  
 
 function switchTurn() {
   if (turn === player1) {
