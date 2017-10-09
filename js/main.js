02eabab4cea647e477b44b6e3e73a5ed3c1d6f13
Patrick Finazzo <pfinazzoproduction@gmail.com>
2017-10-08 23:52:51 -0700

    comment out old code

--- js/main.js
+++ js/main.js
@@ -1,27 +1,6 @@
 $(function () {
-  // much like tic tac toe this game will involve a table and two players. As a user, I would like to pick my color by selecting that color in another table. This color would be the color of my tokens that I try to connect. The main game table will consist of 7 columns and 6 rows. Much of the code for this game will be similar to tic-tac-toe, but one of the key differences is the rule that is normally enforced by gravity in real connect-four. Gravity makes it so that just because a cell is open doesn't men you can place your token there; there has to be a token in the cell below it. The check for winner logic will also have add one more cell because in ttt we try to connect three. 
-
-  // I would like to have two variables player1 and player 2. Our tokens will be represented by jquery.css background-color declarations. We need to make it so selecting a different color in the color table will change the color of whoever's ever turn it is which means we will also need a global variable of 'turn'.
-
-  // We will need a board array that will represnt our gameboard
-  // After that we need to add an jquery .on event listener to the table, so that when we click a td it will run a click function which will push the .css background-color of the current player to the event target which should be that cell. if the event.target is above an empty cell, then push the content to the lowest cell in that colum. This will make it so users actually choose columns instead of cells. After the play has been pushed to the board then the board needs to be rendered so the render function will be called which will push the game board to the DOM. If the board has matched the checkforWinner function then tell the user that whichever user last played has won. Then if someone has won, run the init function clearing the board. 
-
-
-
-
-  //I also will want to add a score count on each side .
-
-
-  //So all in all when a user clicks a cell, it triggers a click function that will push the data to the board array and also apply styling, .css({"background-color": "color"}), to the DOM node thats clicked. If the board array ever matches the winning cases, then trigger an alert 
-
   /*----- constants -----*/
-
-
-
-  var turn;
-
-
-
+  
   /*----- app's state (variables) -----*/
   var board = [
     [0, 0, 0, 0, 0, 0, 0],
@@ -32,20 +11,17 @@ $(function () {
     [0, 0, 0, 0, 0, 0, 0]
   ];
 
-  player1color = "yellow";
-  player2color = "blue";
-  player1 = 1;
-  player2 = 2;
+  // var player1color = "yellow";
+  // var player2color = "blue";
+  var playerMove = '';
+  var player1 = 1;
+  var player2 = 2;
+  var turn;
+  // var blankspace = null;
+  
 
-
-
-
-  blankSpace = null;
-
-
-
-
-
+  
+  
   /*----- cached element references -----*/
 
 
@@ -54,7 +30,7 @@ $(function () {
 
 
   /*----- event listeners -----*/
-  $('table').on('click', 'td', click)
+  $('table').on('click', 'td', click);
 
 
 
@@ -65,63 +41,79 @@ $(function () {
     $('td').css({ "background-color": "white" });
     turn = player1;
     board = [
-      [0, 0, 0, 0, 0, 0, 0]
-      [0, 0, 0, 0, 0, 0, 0]
-      [0, 0, 0, 0, 0, 0, 0]
-      [0, 0, 0, 0, 0, 0, 0]
-      [0, 0, 0, 0, 0, 0, 0]
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0]
     ];
-    render();
-  }
+  };
 
 
 
   function click(event) {
-    var cellTarget = $(event.target);
-    var column = cellTarget.attr("data-col");
-    var row = cellTarget.parent().attr("data-row");
-    cellTarget === player1 ? player1 = ({ "background-color": `${player1color}` }) : player2 = ({ "background-color": `${player2color}` });
-    cellTarget === 0 ? board[row][column] = turn : cellTarget === 1 || 2;
-    // if cellTarget === null ? 
-    // if (turn === player1) {
-    //   cellTarget.css(player1);
-    // } else {
-    //   cellTarget.css(player2);
-    // }
-
-    board[column].forEach(function (i) {
-      turn = player1 ? board[column][i] = player1 : board[column][i] = player2;
-    });
-
-    // console.log(board);
+    var cellTarget = $(event.target); // event target's cell
+    var column = cellTarget.attr("data-col"); // event target's column
+    var row = cellTarget.parent().attr("data-row"); // event target's row
+    console.log(row, column);
+    row = parseInt(row, 10);
+    column = parseInt(column, 10);
+    console.log(board);
+   
     
-    // turn = player1 ? console.log(player1) : console.log(player2);
-    checkBoard(column);
+    
+    console.log(turn);
+    console.log(board);
+
+    // ((cellTarget.css({"background-color": "white"}) && ((board[row][column] === 0)) && (board[(row + 1)][column] === 1 || board[(row + 1)][column] === 2) || (board[(row + 1)][column] !== undefined))) ? board[row][column] = turn : ((board[(row + 1)][column] = turn) && row + 1);
+    // if the cell you click is white (from init) and both the corresponding targeted cell in the board array contains a 0 and the cell below is not zero or undefined (undefined takes care of the situation where we are clicking on a cell in row 6) then the board will have the player value pushed to the board array, otherwise the cell below (row +1) has the data pushed into that square
+    
+
+    // board[row].forEach(function (column) {
+    //   turn === player1 ? board[row][column] = player1 : board[row][column] = player2;
+    // });
+   (board[row][column] === 0) ? (board[row][column] = turn) : (board[row - 1][column] = turn);
+  //  for (var i = 0; i < board[0].length; i++){
+  //   if (board.lastIndexOf(0) === board[5][i]) {
+  //     board[row][column] = 5;
+  //   }
+  //  }
+  if (board[row][column] === 1) {
+    $(this).addClass("player1move");
+  } else if (board[row][column] === 2) {
+    $(this).addClass("player2move");
+  }
+   
+ 
+      
+    console.log(board);
+    // for each element in whatever row has been selected, if the turn is player1 then push player1's data (1) to the corresponding cell in the board array, otherwise (i.e. turn === player2) push player2's data to the board array
+    
+
+    // checkBoard(row, column);
     render();
     switchTurn();
   };
 
 
 function render() {
-  $('td').each(function (idx, elem) {
-    $(elem).css(board[idx])
-  })
-}
+  turn === player1 ? playerMove = "player1move" : playerMove = "player2move";
+};
 
 
 
-function checkBoard(column, elem) {
- for (var i = 0; i < board.length; i++) {
-   if(board[i][column] === 4){
-    $(`[data-row=${i}]>[data-col=${column}]`).addClass("l")
-   };
- };
-  if (elem === 'true') {
-        return elem;
-     };
-    };
-  });
+// function checkBoard(row, column, elem) {
+//  for (var i = 0; i < board[row].length; i++) {
+//    if(board[row][i] === true){
+//     $(`[data-row=${i}]>[data-col=${column}]`).addClass(playerMove)
+//     // ^ runs through the gameboard to find out where which spaces has moved and adds a class of the variable playerMove (which switches between player1move and player2move depending on the state of the variable 'turn') to the clicked table cell 
+//    };
+//  };
+//   if (elem === 'true') {
+//         return elem;
+//      };
+// };
 
 
 function switchTurn() {
@@ -129,9 +121,11 @@ function switchTurn() {
     turn = player2;
   } else {
     turn = player1;
-  }
-  init();
+  } 
 }
+
+init();
+});
 
 
 
