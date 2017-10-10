4caa5fa8ee61adcafcc20e00e3ce328bba757848
Patrick Finazzo <pfinazzoproduction@gmail.com>
2017-10-10 07:40:34 -0700

    data is now moving in array well, colors not moving correctly yet

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@ $(function () {
   var player1 = 1;
   var player2 = 2;
   var turn;
+  var currentColor;
 
   /*----- cached element references -----*/
 
@@ -43,19 +44,100 @@ $(function () {
     var cellTarget = $(event.target); // event target's cell
     var column = cellTarget.attr("data-col"); // event target's column
     var row = cellTarget.parent().attr("data-row"); // event target's row
-    // board[row][column] === true ?  : board[row][column] = turn;
-
-    // if the cell you select is already filled then do nothing, otherwise push the player number held in the turn variable to the corresponding cell in the game board
-    if (board[row][column] === 1 || board[row][column] === 2) {
-      return;
+    row = parseInt(row, 10);
+    column = parseInt(column, 10);
+    if (board[row + 1][column] === 1 || board[row + 1][column] === 2) {
+      if (board[row][column] === 1 || board[row][column] === 2) {
+        return;
+      }
+      console,log(board[row][column])
     } else {
-      board[row][column] = turn;
+      board[row][column] = 0;
+      if (row === 4 && board[5][column] === 0) {
+        board[5][column] = turn;
+        var row = 5;
+      } else if (row === 3 && board[4][column] === 0 && board[5][column] === 0) {
+        board[5][column] = turn;
+        var row  = 5;
+      } else if (row === 2 && board[3][column] === 0 && board[4][column] === 0 && board[5][column] === 0) {
+        board[5][column] = turn;
+        var row  = 5;
+      } else if (row === 1 && board[2][column] === 0 && board[3][column] === 0 && board[4][column] === 0 && board[5][column] === 0) {
+        board[5][column] = turn;
+        var row  = 5;
+      } else if (row === 0 && board[1][column] === 0 && board[2][column] === 0 && board[3][column] === 0 && board[4][column] === 0 && board[5][column] === 0) {
+        board[5][column] = turn;
+        var row  = 5;
+      } else if (row === 4 && board[5][column] === (1 || 2)) {
+        board[4][column] = turn;
+        var row  = 4;
+      } else if (row === 3 && board[4][column] === 0 && board[5][column] === (1 || 2)) {
+        board[4][column] = turn;
+        var row  = 4;
+      } else if (row === 2 && board[3][column] === 0 && board[4][column] === 0 && board[5][column] === (1 || 2)) {
+        board[4][column] = turn;
+        var row  = 4;
+      } else if (row === 1 && board[2][column] === 0 && board[3][column] === 0 && board[4][column] === 0 && board[5][column] === (1 || 2)) {
+        board[4][column] = turn;
+        var row  = 4;
+      } else if (row === 0 && board[1][column] === 0 && board[2][column] === 0 && board[3][column] === 0 && board[4][column] === 0 && board[5][column] === (1 || 2)) {
+        board[4][column] = turn;
+        var row  = 4;
+        } else if (row === 3 && board[4][column] === (1 || 2) && board[5][column] === (1 || 2)) {
+          board[3][column] = turn;
+          var row  = 3;
+        } else if (row === 2 && board[3][column] === 0 && board[4][column] === (1 || 2) && board[5][column] === (1 || 2)) {
+          board[3][column] = turn;
+          var row  = 3;
+        } else if (row === 1 && board[2][column] === 0 && board[3][column] === 0 && board[4][column] === (1 || 2) && board[5][column] === (1 || 2)) {
+          board[3][column] = turn;
+          var row  = 3;
+        } else if (row === 0 && board[1][column] === 0 && board[2][column] === 0 && board[3][column] === 0 && board[4][column] === (1 || 2) && board[3][column] === (1 || 2)) {
+          board[3][column] = turn;
+          var row  = 3;
+        } else if (row === 2 && board[3][column] === (1 || 2) && board[4][column] === (1 || 2) && board[5][column] === (1 || 2)) {
+          board[2][column] = turn;
+          var row  = 3;
+        } else if (row === 1 && board[2][column] === 0 && board[3][column] === (1 || 2) && board[4][column] === (1 || 2) && board[5][column] === (1 || 2)) {
+          board[2][column] = turn;
+          var row  = 2;
+        } else if (row === 0 && board[1][column] === 0 && board[2][column] === 0 && board[3][column] === (1 || 2) && board[4][column] === (1 || 2) && board[5][column] === (1 || 2)) {
+          board[2][column] = turn;
+          var row  = 2;
+        } else if (row === 1 && board[2][column] === (1 || 2) && board[3][column] === (1 || 2) && board[4][column] === (1 || 2) && board[5][column] === (1 || 2)) {
+          board[1][column] = turn;
+          var row  = 1;
+        } else if (row === 0 && board[1][column] === 0 && board[2][column] === (1 || 2) && board[3][column] === 0 && board[4][column] === (1 || 2) && board[3][column] === (1 || 2)) {
+          board[1][column] = turn;
+          var row  = 1;
+        } else if (row === 0 && board[1][column] === (1 || 2) && board[2][column] === (1 || 2) && board[3][column] === (1 || 2) && board[4][column] === (1 || 2) && board[5][column] === (1 || 2)) {
+          board[0][column] = turn;
+          var row  = 1;
+        };
+        row.toString();
     }
-    (board[row][column] === 1) ? $(this).css({ "background-color": `${player1color}`}) : $(this).css({ "background-color": `${player2color}`});
+    var newCellTarget = cellTarget.parent().attr("data-row", row)
+    
+   // parse ints are converting strings into numbers with base 10
+  
+if (board[row][column] === 1) {
+  $(this).css({ "background-color": `${player1color}`}) 
+} else if (board[row][column] === 0) {
+  $(this).css({ "background-color": `${player2color}`});
+};
     console.log(board);
       switchTurn();
     };
   
+
+  function colorSelect() {
+      if (turn === player1) {
+        currentColor = player1;
+      } else {
+        currentColor = player2;
+      } 
+    }
+
 
 function switchTurn() {
   if (turn === player1) {
@@ -63,7 +145,9 @@ function switchTurn() {
   } else {
     turn = player1;
   } 
+  colorSelect();
 }
+
 
 init();
 });
